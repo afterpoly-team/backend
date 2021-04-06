2d0c322b8dcd386003ec5bb4abc519d3847f671a
evgenii-podruzhinskii <evg.pod.dm@gmail.com>
2021-04-06 21:57:55 +0300

    Add pagination support

--- backend/drf_defaults.py
+++ backend/drf_defaults.py
@@ -7,6 +7,12 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class DefaultResultsSetPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 5
     page_size_query_param = 'page_size'
     max_page_size = 100000
+
+    def get_paginated_response(self, data):
+        response = super(DefaultResultsSetPagination,
+                         self).get_paginated_response(data)
+        response.data['total_pages'] = self.page.paginator.num_pages
+        return response
